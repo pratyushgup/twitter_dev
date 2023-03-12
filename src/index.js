4509072021039e637f6555b520a027cfed6c8416
Pratyush Gupta <pg0623733@gmail.com>
2023-03-13 00:25:01 +0530

    Added v1 for tweet and hashtag model

--- src/index.js
+++ src/index.js
@@ -2,21 +2,10 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
-// const Tweet = require('./models/tweet');
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
+
+
 app.listen(3000,async ()=>{
     console.log('Server Started');
     await connect();
-    console.log('MongoDB connected');
-    // const tweet = await Tweet.create({
-    //     content:'Third Tweet',
-    //     userEmail:'b@c.com'
-    // });
-    // const tweets = await Tweet.find({userEmail:'a@b.com'});
-    const repo = new TweetRepository();
-    const tweet = await repo.create({content:'new tweet with hook is here and what else tell'})
-    console.log(tweet);
-    // const tweet = await repo.getAll(0,4);
-    // console.log(tweet[0].contentWithEmail);
+    
 })    
